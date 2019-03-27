aa306bd2d522841585b1125678743f84bf57b900
Aasim Kandrikar <aasimk@umich.edu>
2019-03-26 22:39:12 -0400

    Added a couple more packages. Cleaned up init.el

--- init.el
+++ init.el
@@ -5,34 +5,18 @@
 ;; Loads Org file and some basic setup
 
 ;;; Code:
+;; Package configs
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("org"   . "http://orgmode.org/elpa/")
+                         ("gnu"   . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
 
-(let ((gc-cons-threshold most-positive-fixnum))
+;; Bootstrap `use-package`
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
 
-  ;; Set repositories
-  (require 'package)
-  (setq-default
-   load-prefer-newer t
-   package-enamble-at-startup nil)
-  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
-  (package-initialize)
-
-  ;; Install dependencies
-  (unless (and (package-installed-p 'delight)
-	       (package-installed-p 'use-package))
-    (package-refresh-contents)
-    (package-install 'delight t)
-    (package-install 'use-package t))
-  (setq-default
-   use-package-always-defer t
-   use-package-always-ensure t)
-
-  ;; Use latest Org mode
-  (use-package org
-    :ensure org-plus-contrib)
-
-  (org-babel-load-file (expand-file-name "bootstrap.org" user-emacs-directory))
-
-  (garbage-collect))
-
-;;; init.el ends here
+(org-babel-load-file (expand-file-name "bootstrap.org" user-emacs-directory))
